629273a02885476d21a8f40ac1365537ad7f3652
Sergei Garcia <sgarcia.dev@gmail.com>
2017-10-17 22:32:31 -0500

    Removed Skeleton, Created custom CSS grid system

--- src/common/wow.service.js
+++ src/common/wow.service.js
@@ -3,7 +3,10 @@ import {WOW} from 'wowjs';
 class WowService {
     constructor() {
         this.WOW = new WOW({
-            offset: 100
+            // offset: 300,
+            boxClass: 'reveal',
+            mobile: true,
+            live: false
         });
         this.WOW.init();
     }
