e6d6b3b8cb3d804be9eeb2bb23749b464d339e5f
Sergei Garcia Ortega <sgarcia.dev@gmail.com>
2018-11-08 20:53:12 -0600

    Reorg project

---
+++ script.js
@@ -0,0 +1,32 @@
+const titles = [
+  "Fullstack Developer @ Luxoft GDL",
+  "Mentor @ Thinkful.com",
+  "Web Design Enthusiast",
+  "JS Fanatic",
+  "Performance Advocate"
+];
+const devEl = document.querySelector("#dev");
+let currentTitle = 0;
+
+setTimeout(() => {
+  animate(titles[currentTitle]);
+  currentTitle++;
+
+  window.setInterval(() => {
+    animate(titles[currentTitle]);
+    currentTitle = currentTitle < titles.length - 1 ? currentTitle + 1 : 0;
+  }, 6000);
+}, 1000);
+
+function animate(newTitle) {
+  let letter = 1;
+  const id = setInterval(() => {
+    requestAnimationFrame(() => {
+      devEl.innerHTML = newTitle.substr(0, letter);
+      letter++;
+      if (letter > newTitle.length) {
+        clearInterval(id);
+      }
+    });
+  }, 30);
+}
